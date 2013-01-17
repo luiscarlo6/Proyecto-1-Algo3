75e050286ef71bed7486b2f951328e07ddc33114
Doubtless <luiscarlo6@gmail.com>
2013-01-17 17:28:23 -0430

    Nodo y Main

--- Main.java
+++ Main.java
@@ -5,7 +5,24 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Nodo A;
+		int max = 1000;
+		int colisiones = 0;
+		boolean arr[] = new boolean[max];
+		
+		for (int i = 0; i!=max-250 ; i++){
+			A = new Nodo("Nodo_"+i);
+//			System.out.println(A.hashCode()%max);
+			if (arr[A.hashCode()%max]==false){
+				arr[A.hashCode()%max] = true;
+			}
+			else{
+				colisiones++;
+			}
+			
+			
+		}
+		System.out.println("Colisiones" + colisiones);
 
 	}
 
